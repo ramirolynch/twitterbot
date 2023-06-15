33dd6ad12b0e66fba0554b7627f7c215355af76e
Ramiro Lynch <ramirolynch6@gmail.com>
2023-06-15 11:17:37 -0500

    added GreetingController

---
+++ src/main/java/com/twitterbot/controllers/GreetingController.java
@@ -0,0 +1,18 @@
+package com.twitterbot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
