b5898e0699cb3a480a715022cb8ae26d6b5fa087
Ramiro Lynch <ramirolynch6@gmail.com>
2023-06-17 16:57:38 -0500

    added timeout

--- src/main/java/com/twitterbot/SchedulingConfig.java
+++ src/main/java/com/twitterbot/SchedulingConfig.java
@@ -1,7 +1,5 @@
 package com.twitterbot;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -23,10 +21,11 @@ public class SchedulingConfig {
 	private ChatGptService chatGptService;
 
 	// method in service class, annotated with fixed time and time unit
-	@Scheduled(cron="0 0/30 8-10 * * *")
-	public void startBot() {
+	@Scheduled(fixedDelayString="PT10H", initialDelay=10000L)
+	public void startBot() throws InterruptedException {
 		String joke = chatGptService.getJoke("write a fact about Java, for learning purposes");
 		tweetService.sendTweet(joke);
 		System.out.println("Tweet sent");
+		Thread.sleep(10000L);
 	}
 }
