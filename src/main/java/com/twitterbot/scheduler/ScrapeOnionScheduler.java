fd39f9a48849526b16bc774b88f06e2608f8b07c
Ramiro Lynch <ramirolynch6@gmail.com>
2023-08-26 22:52:39 -0500

    changed cron

--- src/main/java/com/twitterbot/scheduler/ScrapeOnionScheduler.java
+++ src/main/java/com/twitterbot/scheduler/ScrapeOnionScheduler.java
@@ -28,7 +28,7 @@ public class ScrapeOnionScheduler {
 	@Autowired
 	TweetRepository tweetRepository;
 	
-	@Scheduled(cron = "0 51 3 * * 6")
+	@Scheduled(cron = "0 54 3 * * 7")
 	public int scrapeOnionHeadlines() throws InterruptedException  {
 
 		List<String> headlines = scrapingService.scrapeHeadlines("https://www.theonion.com/");
